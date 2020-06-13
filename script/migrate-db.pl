45999ead8341958b9e13948f9d33d4ed3cb18e5e
Gene Boggs <gene@ology.net>
2020-06-13 02:42:25 -0700

    Get the db from the config

--- script/migrate-db.pl
+++ script/migrate-db.pl
@@ -9,7 +9,9 @@ use Schema;
 
 my $dbh = DBI->connect('DBI:SQLite:dbname=../ShopList/shoplist.db', '', '') or die $DBI::errstr;
 
-my $schema = Schema->connect('DBI:SQLite:dbname=shopping_list.db', '', '');
+my $config = do 'shopping_list.conf';
+
+my $schema = Schema->connect($config->{database}, '', '');
 
 my $sql = 'SELECT * FROM shop_list where account_id=2';
 my $sth = $dbh->prepare($sql) or die $dbh->errstr;
