48ee8d2f04b93f943c9d8c6edd3869133b9f44b1
Gene Boggs <gene@ology.net>
2020-06-16 12:53:37 -0700

    Update migration

--- migration-20200616.sql
+++ migration-20200616.sql
@@ -1,7 +1,8 @@
+DROP TABLE item_counts;
 CREATE TABLE item_counts (
   id INTEGER PRIMARY KEY NOT NULL,
-  count int,
-  item_id int NOT NULL,
+  count INTEGER NOT NULL,
+  item_id INTEGER NOT NULL,
   FOREIGN KEY (item_id) REFERENCES items(id)
 );
 CREATE INDEX item_counts_idx_item_id ON item_counts (item_id);
